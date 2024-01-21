267a9890ecad73268bf93b6e835ac556a3e0a4ee
The Luke <100490693+Luki013@users.noreply.github.com>
2024-01-21 19:25:19 -0300

    Add Shader class

---
+++ src/Shader.h
@@ -0,0 +1,40 @@
+#ifndef SHADER_CLASS_H
+#define SHADER_CLASS_H
+
+#include <string>
+#include <unordered_map>
+
+struct ShaderProgramSource
+{
+	std::string vertexShader;
+	std::string fragmentShader;
+
+};
+
+class Shader
+{
+private:
+	std::string m_FilePath;
+	unsigned int m_RendererID;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
+
+public:
+	Shader(const std::string& filePath);
+	~Shader();
+
+	void Bind();
+	void Unbind();
+
+	void SetUniform1f(const std::string& name, float value);
+	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+
+private:
+	ShaderProgramSource ParseShader(const std::string& filePath);
+	unsigned int CompileShader(unsigned int type, const std::string& source);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+
+	unsigned int GetUniformLocation(const std::string& name);
+
+};
+
+#endif
