e42831d0ba0ebcb4842b81bd1d8074bc5e3e33fc
The Luke <100490693+Luki013@users.noreply.github.com>
2024-01-30 01:20:51 -0300

    add test menu

--- src/tests/Test.h
+++ src/tests/Test.h
@@ -1,6 +1,9 @@
 #ifndef TEST_BASE_CLASS_H
 #define TEST_BASE_CLASS_H
 
+#include <vector>
+#include <functional>
+
 namespace test
 {
 	class Test
@@ -12,6 +15,27 @@ namespace test
 		virtual void OnUpdate(float deltaTime) {}
 		virtual void OnRender() {}
 		virtual void OnImGuiRender() {}
+
+	};
+
+	class TestMenu : public Test
+	{
+	public:
+		TestMenu(Test*& currentTestPointer);
+
+		void OnImGuiRender() override;
+
+		template<typename T>
+		void RegisterTest(const std::string& name)
+		{
+			std::cout << "Registering test " << name << std::endl;
+
+			m_Tests.push_back(std::make_pair(name, []() { return new T(); }));
+		}
+
+	private:
+		Test*& m_CurrentTest;
+		std::vector<std::pair<std::string, std::function<Test* ()>>> m_Tests;
 
 	};
 }
